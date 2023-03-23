1a2e38f55f8a825e85b2f8fa30537e26ecd6344c
Amirmahdi1380 <nourkazemi80@gmail.com>
2023-03-23 22:20:21 +0430

    add snackbar for google login #3

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   LoginScreen({super.key});
 
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   final _usernameTextController = TextEditingController(text: 'amirahmadi');
+
   final _passwordTextController = TextEditingController(text: '12345678');
 
   @override
@@ -13,138 +19,161 @@ class LoginScreen extends StatelessWidget {
       home: Scaffold(
         resizeToAvoidBottomInset: false,
         backgroundColor: Colors.lightBlue[100],
-        body: SafeArea(
-          child: Column(
-            children: [
-              Expanded(
-                flex: 1,
-                child: Image.asset('assets/images/sticky_note.png'),
-              ),
-              Expanded(
-                flex: 3,
-                child: Container(
-                  margin: const EdgeInsets.all(20),
-                  padding: const EdgeInsets.all(20),
-                  decoration: const BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.all(Radius.circular(20))),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      TextField(
-                        controller: _usernameTextController,
-                        decoration: InputDecoration(
-                          labelText: 'Email',
-                          labelStyle: TextStyle(
-                              fontFamily: 'sm',
-                              fontSize: 18,
-                              color: Colors.black),
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(20),
-                            ),
-                            borderSide:
-                                BorderSide(color: Colors.black, width: 2),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(20),
-                            ),
-                            borderSide:
-                                BorderSide(color: Colors.blue, width: 2),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      TextField(
-                        controller: _passwordTextController,
-                        decoration: InputDecoration(
-                          labelText: 'Password',
-                          labelStyle: TextStyle(
-                              fontFamily: 'sm',
-                              fontSize: 18,
-                              color: Colors.black),
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(20),
-                            ),
-                            borderSide:
-                                BorderSide(color: Colors.black, width: 2),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(20),
-                            ),
-                            borderSide:
-                                BorderSide(color: Colors.blue, width: 2),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      OutlinedButton(
-                        style: ElevatedButton.styleFrom(
-                            primary: Colors.lightBlue[100],
-                            textStyle:
-                                TextStyle(fontFamily: 'sb', fontSize: 18),
-                            minimumSize: Size(200, 48),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20))),
-                        onPressed: () {
-                          print('object');
-                        },
-                        child: Text('Login'),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      OutlinedButton(
-                        style: OutlinedButton.styleFrom(
-                            minimumSize: Size(342, 64)),
-                        onPressed: () {},
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Image.asset('assets/images/google.png'),
-                            SizedBox(
-                              width: 16,
-                            ),
-                            Text(
-                              'login with your google account',
-                              style: TextStyle(
-                                  fontFamily: 'IS', color: Colors.black),
-                            ),
-                          ],
-                        ),
-                      ),
-                      SizedBox(
-                        height: 184,
-                        child:
-                            Image.asset('assets/images/login_ilustratiuon.jpg'),
-                      ),
-                      OutlinedButton(
-                        style: ElevatedButton.styleFrom(
-                            primary: Colors.lightBlue[100],
-                            textStyle:
-                                TextStyle(fontFamily: 'sb', fontSize: 18),
-                            minimumSize: Size(200, 48),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20))),
-                        onPressed: () {
-                          print('object');
-                        },
-                        child: Text('I don' 't have account'),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
+        body: getLogin(
+          usernameTextController: _usernameTextController,
+          passwordTextController: _passwordTextController,
         ),
+      ),
+    );
+  }
+}
+
+class getLogin extends StatelessWidget {
+  const getLogin({
+    Key? key,
+    required TextEditingController usernameTextController,
+    required TextEditingController passwordTextController,
+  })  : _usernameTextController = usernameTextController,
+        _passwordTextController = passwordTextController,
+        super(key: key);
+
+  final TextEditingController _usernameTextController;
+  final TextEditingController _passwordTextController;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Image.asset('assets/images/sticky_note.png'),
+          ),
+          Expanded(
+            flex: 3,
+            child: Container(
+              margin: const EdgeInsets.all(20),
+              padding: const EdgeInsets.all(20),
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(20))),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  TextField(
+                    controller: _usernameTextController,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      labelStyle: TextStyle(
+                          fontFamily: 'sm', fontSize: 18, color: Colors.black),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20),
+                        ),
+                        borderSide: BorderSide(color: Colors.black, width: 2),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20),
+                        ),
+                        borderSide: BorderSide(color: Colors.blue, width: 2),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextField(
+                    controller: _passwordTextController,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      labelStyle: TextStyle(
+                          fontFamily: 'sm', fontSize: 18, color: Colors.black),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20),
+                        ),
+                        borderSide: BorderSide(color: Colors.black, width: 2),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20),
+                        ),
+                        borderSide: BorderSide(color: Colors.blue, width: 2),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  OutlinedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.lightBlue[100],
+                      textStyle: TextStyle(fontFamily: 'sb', fontSize: 18),
+                      minimumSize: Size(200, 48),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    onPressed: () {
+                      print('object');
+                    },
+                    child: Text(
+                      'Login',
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(minimumSize: Size(342, 64)),
+                    onPressed: () {
+                      final snackBar = SnackBar(
+                        content: const Text('next updates will add!'),
+                        backgroundColor: (Colors.black),
+                        action: SnackBarAction(
+                          label: 'dismiss',
+                          onPressed: () {},
+                        ),
+                      );
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset('assets/images/google.png'),
+                        SizedBox(
+                          width: 16,
+                        ),
+                        Text(
+                          'login with your google account',
+                          style:
+                              TextStyle(fontFamily: 'IS', color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 184,
+                    child: Image.asset('assets/images/login_ilustratiuon.jpg'),
+                  ),
+                  OutlinedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.lightBlue[100],
+                        textStyle: TextStyle(fontFamily: 'sb', fontSize: 18),
+                        minimumSize: Size(200, 48),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20))),
+                    onPressed: () {
+                      print('object');
+                    },
+                    child: Text('I don' 't have account'),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
