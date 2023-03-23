d74a1b932e296fb1527fbb02e541a3debb7bda90
Amirmahdi1380 <nourkazemi80@gmail.com>
2023-03-23 19:10:27 +0430

    login screen #1

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:note/screen/login_screen.dart';
+
+void main() {
+  runApp(LoginScreen());
+}
---
+++ lib/screen/login_screen.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  LoginScreen({super.key});
+
+  final _usernameTextController = TextEditingController(text: 'amirahmad');
+  final _passwordTextController = TextEditingController(text: '12345678');
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Directionality(
+        textDirection: TextDirection.ltr,
+        child: Scaffold(
+          resizeToAvoidBottomInset: false,
+          backgroundColor: Colors.lightBlue[100],
+          body: Column(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Text('data'),
+              ),
+              Expanded(
+                flex: 3,
+                child: Container(
+                  margin: const EdgeInsets.all(20),
+                  padding: const EdgeInsets.all(20),
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(20))),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      TextField(
+                        controller: _usernameTextController,
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          labelStyle: TextStyle(
+                              fontFamily: 'sm',
+                              fontSize: 18,
+                              color: Colors.black),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20),
+                            ),
+                            borderSide:
+                                BorderSide(color: Colors.black, width: 2),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20),
+                            ),
+                            borderSide:
+                                BorderSide(color: Colors.blue, width: 2),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      TextField(
+                        controller: _passwordTextController,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          labelStyle: TextStyle(
+                              fontFamily: 'sm',
+                              fontSize: 18,
+                              color: Colors.black),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20),
+                            ),
+                            borderSide:
+                                BorderSide(color: Colors.black, width: 2),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20),
+                            ),
+                            borderSide:
+                                BorderSide(color: Colors.blue, width: 2),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      OutlinedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.lightBlue[100],
+                            textStyle:
+                                TextStyle(fontFamily: 'sb', fontSize: 18),
+                            minimumSize: Size(200, 48),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20))),
+                        onPressed: () {
+                          print('object');
+                        },
+                        child: Text('Login'),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                            minimumSize: Size(342, 64)),
+                        onPressed: () {},
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset('assets/images/google.png'),
+                            SizedBox(
+                              width: 16,
+                            ),
+                            Text(
+                              'با گوگل وارد شوید',
+                              style: TextStyle(
+                                  fontFamily: 'IS', color: Colors.black),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                          height: 220,
+                          child: Image.asset(
+                              'assets/images/login_ilustratiuon.jpg'))
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
